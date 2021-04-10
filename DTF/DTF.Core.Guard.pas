a2dcfe566b7843a696e0fc827b10bd2dcf6b3932
Humphrey Kim <hjfactory@gmail.com>
2021-04-10 22:28:38 +0900

    Authorization, Authentication 구상

---
+++ DTF/DTF.Core.Guard.pas
@@ -0,0 +1,23 @@
+unit DTF.Core.Guard;
+
+interface
+
+// Authentification
+  // guard
+    // Session guard
+
+// Authorization
+  // View 별 인가
+  // 기능(Action) 별 인가
+
+  // Policy Injection
+
+  // Autorize()
+
+  // AuthService
+    // Allows
+    // Denies
+
+implementation
+
+end.
